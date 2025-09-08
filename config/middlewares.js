5fb828957490c45cfac0da9846eec99a40e5f677
lucky cungwa <cungwalucky@gmail.com>
2025-09-08 21:20:52 +0200

    update for conneccting frontend origin

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,19 +2,18 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000', 'https://human-element.co.za', 'https://www.human-element.co.za'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['http://localhost:3000', 'https://human-element.co.za', 'https://www.human-element.co.za'], 
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization'],
-    },
-  },
 ];
